cf774bf053ff6093d3e4ccdbe82ef88498e2e03d
shine <xiaoshaoqq@gmial.com>
2018-08-08 23:08:34 +0800

    add components

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-
+import { App} from './containers/children';
 class Root extends Component{
 
     render() {
         return (
             <div>
                 this is root
+                <App />
             </div>
         );
     }
---
+++ src/containers/App/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './style.less';
+export default class App extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <h1>APP</h1>
+            </div>
+        );
+    }
+}
