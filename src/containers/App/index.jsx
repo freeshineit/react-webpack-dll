f51506dc6443bb52d053ca49b6657f68619252cc
shine <xiaoshaoqq@gmail.com>
2018-11-10 15:38:57 +0800

    antd test

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,27 +1,74 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import { renderRoutes } from 'react-router-config'
-import logo from '../../public/images/logo.svg';
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import { renderRoutes } from 'react-router-config';
+// import logo from '../../public/images/logo.svg';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+
 import './style.less';
 
 class App extends Component {
-    render() {
-        const {route} = this.props;
 
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className='App-logo' />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <Link to='/'>Home</Link>
-                <Link to='/data'>Data</Link>
-                {
-                    renderRoutes(route.routes)
-                }
-            </div>
-        );
+    constructor(props) {
+        super(props)
+        this.state = {
+            collapsed: false
+        };
     }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+  render() {
+    const { route } = this.props;
+
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className='logo' />
+          <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+            <Menu.Item key='1'>
+              <Icon type='user' />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key='2'>
+              <Icon type='video-camera' />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key='3'>
+              <Icon type='upload' />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className='trigger'
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280
+            }}
+          >
+            Content
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
-export default App
+export default App;
